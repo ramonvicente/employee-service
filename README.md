5c5d0c17a63aa6d981bfef22e4ed74e7e888ca20
Ramon Vicente <ramonvicentee@gmail.com>
2023-07-03 21:28:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Employee Service
+This project is an application that handle with employee information of a company.
+
---
+++ src/test/java/com/ramonvicente/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonvicente.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
