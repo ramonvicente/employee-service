f6de6139923f221b6bea8688314f8b291b7c5bd6
Ramon Vicente <ramonvicentee@gmail.com>
2023-07-05 21:03:02 +0200

    wip: refactoring implementation

--- src/main/java/com/ramonvicente/employeeservice/message/MessageConsumer.java
+++ src/main/java/com/ramonvicente/employeeservice/message/MessageConsumer.java
@@ -1,15 +1,14 @@
 package com.ramonvicente.employeeservice.message;
 
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Component
 @Slf4j
 public class MessageConsumer {
 
-    @KafkaListener(topics = "employee-topic", groupId = "employee-service-group-id")
+    @KafkaListener(topics = "${employee.service.event.producer.topic}", 
+                    groupId = "${spring.kafka.consumer.group-id}")
     public void listen(String message) {
         log.info(message);
     }
